0bdf9017f2107b505eeffdbc1b1db5d5d29bdc4c
John Resig <jeresig@gmail.com>
2025-12-31 14:57:27 -0500

    Support Ink file includes.

--- src/engine/build/ink-loader.js
+++ src/engine/build/ink-loader.js
@@ -1,6 +1,25 @@
-import { Compiler } from "inkjs/full";
+import fs from "node:fs";
+import path from "node:path";
 
-export default (source) => {
-    const story = new Compiler(source).Compile();
+import { Compiler, CompilerOptions } from "inkjs/full";
+
+export default function (source) {
+    const fileName = this.resourcePath;
+    const baseDir = path.dirname(fileName);
+
+    // Load imported ink files
+    const options = new CompilerOptions(null, [], false, null, {
+        ResolveInkFilename: (filename) => {
+            return path.resolve(baseDir, filename);
+        },
+        LoadInkFileContents: (filename) => {
+            // Make sure that Rspack knows about the dependency so that it
+            // can rebuild the game when the imported file changes.
+            this.addDependency(filename);
+            return fs.readFileSync(filename, "utf8");
+        },
+    });
+
+    const story = new Compiler(source, options).Compile();
     return `module.exports = ${story.ToJson()}`;
-};
+}
